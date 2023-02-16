27c548f0d56245a92d2b6846d501e0b892c0da79
Felixdiamond <diamondfelix006@gmail.com>
2023-02-16 12:19:21 -0800

    bankai

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- Lists all records in descending order of scores
-SELECT * FROM second_table
+-- Lists score and name records in descending order of scores
+SELECT score, name FROM second_table
 ORDER BY score DESC;
