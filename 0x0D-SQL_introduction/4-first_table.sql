961360b8ca5cb60c92054967ca9a8cf4cc3024f7
Felixdiamond <diamondfelix006@gmail.com>
2023-02-16 09:56:20 -0800

    bankai

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Creates Table named first_table
-CREATE TABLE IF NOT EXISTS first_name (
-	id int,
-	name varchar(255)
+CREATE TABLE IF NOT EXISTS `first_name` (
+	`id` int,
+	`name` varchar(255)
 );
