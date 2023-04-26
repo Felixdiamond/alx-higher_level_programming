2693a2c66504b0cf6c9b7640f42370abd7e48c9c
Felix Dawodu <75272879+Felixdiamond@users.noreply.github.com>
2023-04-26 01:19:20 +0100

    Anya

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Get the API URL from the command-line arguments
+// Get url and set characterId
 const url = process.argv[2];
+const characterId = '18';
 
-// ID of the character "Wedge Antilles"
-const characterId = 18;
-const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
-// Make a request to the movies API endpoint
-request.get(url, (error, response, body) => {
-  if (error) {
-    return;
-  }
-  if (response.statusCode !== 200) {
-    return;
-  }
-
-  // Parse the response body as JSON
-  const moviedataContent = JSON.parse(body);
-
-  // Filter the movies based on the presence of the character "Wedge Antilles"
-  const characterMovies = moviedataContent.results.reduce((count, movie) => {
-    return movie.characters.includes(characterUrl) ? count + 1 : count;
-  }, 0);
-
-  // Print the total number of movies where "Wedge Antilles" appears
-  console.log(characterMovies);
+// Make a request to the API
+request(url, function (error, response, body) {
+if (error) {
+console.error(error);
+} else {
+const movies = JSON.parse(body).results;
+const count = movies.filter(movie => movie.characters.some(characterUrl => characterUrl.includes(characterId))).length;
+console.log(count);
+}
 });
