ea43f2df0e920078ee37cc6bc3799abeb808a6a8
Felixdiamond <diamondfelix006@gmail.com>
2023-04-25 17:22:51 -0700

    Anya gets a bankai

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,11 +7,11 @@ const characterId = '18';
 
 // Make a request to the API
 request(url, function (error, response, body) {
-if (error) {
-console.error(error);
-} else {
-const movies = JSON.parse(body).results;
-const count = movies.filter(movie => movie.characters.some(characterUrl => characterUrl.includes(characterId))).length;
-console.log(count);
-}
+  if (error) {
+    console.error(error);
+  } else {
+    const movies = JSON.parse(body).results;
+    const count = movies.filter(movie => movie.characters.some(characterUrl => characterUrl.includes(characterId))).length;
+    console.log(count);
+  }
 });
